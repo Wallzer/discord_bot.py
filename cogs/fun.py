9a6961738c7121f90f504b0344d3c0f9f483f04b
Wallzer <35204495+Wallzer@users.noreply.github.com>
2024-11-28 00:31:45 +0200

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -283,7 +283,7 @@ class Fun_Commands(commands.Cog):
         await msg.edit(content=f"> {result}\nBetter luck next time")
 
     @commands.command()
-    async def kys(self,ctx:CustomContext)
+    async def kys(self,ctx:CustomContext):
         kys='pls kill urself'
         await ctx.send(f'Hello nigger {kys}')
 
